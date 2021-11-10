9b2c66da6fd37b3871b11b37e63f4e46e144a769
PuneethKumarA <apuneethk@gmail.com>
2021-11-09 21:38:56 -0800

    dependency injection -commit

---
+++ DXCjava/src/dependency/injection/Jioservice.java
@@ -0,0 +1,12 @@
+package dependency.injection;
+
+public class Jioservice implements Service {
+
+		@Override
+		public String getInfo() {
+			// TODO Auto-generated method stub
+			return "jio services";
+		}
+
+	}
+
